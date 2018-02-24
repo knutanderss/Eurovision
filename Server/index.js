31fa68a3da22a7268436d9a5c515d7e809be07cd
Knut Anders Stokke <knutandersstokke@gmail.com>
2018-02-24 19:55:30 +0100

    Server with connection to mongodb, route for /artists

---
+++ Server/index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const app = express();
+var MongoClient = require('mongodb').MongoClient
+var bodyParser = require('body-parser');
+var cors = require('cors')
+
+app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.text());
+
+connectToDb = (callback) => {
+    MongoClient.connect('mongodb://localhost:27017', function (err, client) {
+        if (err) throw err;
+        var db = client.db('eurovision');
+        callback(db);
+    });
+}
+
+sendBackArtists = (res) => {
+    connectToDb((db) => db.collection('artists').find().toArray((err, result) => {
+        res.send(JSON.stringify(result));
+    }));
+}
+
+let i = 0;
+
+app.get('/number', (req, res) => {
+    i++;
+    let s = "Number of requests: " + i;
+    console.log(s);
+    res.send(s);
+});
+
+app.get('/artists', (req, res) => {
+    sendBackArtists(res);
+});
+
+app.post('/artists', (req, res) => {
+    console.log("Client inserts: " + req.body);
+    connectToDb((db) => db.collection('artists').insert({name: req.name, country: req.country}));
+    sendBackArtists(res);
+});
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
