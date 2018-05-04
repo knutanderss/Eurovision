fd4d32afacdc047b54e98eabfcb9de3cf8070a99
Knut Anders Stokke <knutandersstokke@gmail.com>
2018-05-04 03:08:04 +0200

    update server url

--- client/src/assets/constants.js
+++ client/src/assets/constants.js
@@ -3,6 +3,6 @@ import {Platform} from 'react-native';
 const BACKGROUND_COLOR = '#1C2EA0';
 const IOS = Platform.OS === 'ios';
 const TITLE_FONT = IOS ? 'American Typewriter' : 'serif';
-const SERVER_URL = 'http://localhost:3000';
+const SERVER_URL = 'http://eurovisionapp.herokuapp.com';
 
 export {BACKGROUND_COLOR, IOS, TITLE_FONT, SERVER_URL};
