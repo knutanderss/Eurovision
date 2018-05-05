50973b68d5efc121d955eb0235308a2d775a7082
Knut Anders Stokke <knutandersstokke@gmail.com>
2018-05-05 16:36:59 +0200

    fix

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -37,9 +37,7 @@ export function requestArtists () {
           payload: result,
         });
       })
-      .catch (error => {
-        console.log (error);
-      });
+      .catch (console.log);
   };
 }
 
