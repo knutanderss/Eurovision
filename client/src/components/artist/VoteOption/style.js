4abf37ed0a9d0e174b0c31c51bbcba28536eab7f
Knut Anders Stokke <knutandersstokke@gmail.com>
2018-05-12 19:18:25 +0200

    fix voteoption for iphone 5

--- client/src/components/artist/VoteOption/style.js
+++ client/src/components/artist/VoteOption/style.js
@@ -1,21 +1,25 @@
-import { StyleSheet, Platform } from "react-native"
-import { TITLE_FONT } from "../../../assets/constants"
+import {StyleSheet, Platform, Dimensions} from "react-native"
+import {TITLE_FONT} from "../../../assets/constants"
+const {height, width} = Dimensions.get('window');
 
 const ios = Platform.OS === "ios"
+const smallScreen = width < 321;
 
 export default StyleSheet.create({
-    container: {
-        paddingHorizontal: 32,
-        paddingVertical: 12,
-        flexDirection: "row",
-        justifyContent: "space-between",
-        alignItems: "center"
-    },
-    voteText: {
-        fontFamily: "Avenir",
-        color: "#000",
-        fontSize: 16,
-        lineHeight: 24,
-        fontWeight: "800"
-    }
+  container: {
+    paddingHorizontal: 32,
+    paddingVertical: 12,
+    flexDirection: smallScreen
+      ? "column"
+      : "row",
+    justifyContent: "space-between",
+    alignItems: "center"
+  },
+  voteText: {
+    fontFamily: "Avenir",
+    color: "#000",
+    fontSize: 16,
+    lineHeight: 24,
+    fontWeight: "800"
+  }
 })
