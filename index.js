381aa75939808e38c0f765cdf5516071f462384e
Knut Anders Stokke <knutandersstokke@gmail.com>
2018-05-12 12:20:07 +0200

    Init websocket before routing

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const configureMiddleware = require('./Server/src/middleware')
-const configureRoutes = require('./Server/src/route')
 const configureWebSocket = require('./Server/src/websocket')
+const configureRoutes = require('./Server/src/route')
 
 configureMiddleware(app)
-configureRoutes(app)
 configureWebSocket(app)
+configureRoutes(app)
 
 const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
